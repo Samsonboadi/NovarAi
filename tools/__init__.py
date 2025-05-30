014933aa0c000224b8a09d90bc0c0892d57c5fc6
Samson <boadisamson@yahoo.com>
2025-05-31 01:14:49 +0200

    Separated running tools from main

--- tools/__init__.py
+++ tools/__init__.py
@@ -1,10 +1,12 @@
 from .kadaster_tool import KadasterBRKTool, ContactHistoryTool
 from .pdok_location import find_location_coordinates, search_dutch_address_pdok, pdok_service,test_pdok_integration
+from .pdok_building_tool import PDOKBuildingsRealTool
 __all__ = [
     "KadasterBRKTool",
     "ContactHistoryTool",
     "find_location_coordinates",
     "search_dutch_address_pdok",
     "pdok_service", 
-    "test_pdok_integration"
+    "test_pdok_integration",
+    "PDOKBuildingsRealTool"
 ]
